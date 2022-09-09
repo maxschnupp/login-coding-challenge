469633fe6376ee772b67bd9af11389bdd5e80930
maxschnupp <maxschnupp@gmail.com>
2022-09-09 20:28:19 +0200

    fixes rerender issues and related performance problemos

--- login-app/src/LoginPage.tsx
+++ login-app/src/LoginPage.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 import LoginCard from "./components/LoginCard";
 import InputColumn from "./components/InputColumn";
-import { isMobile, isTablet } from "react-device-detect";
 import Typography from "./components/Typography";
 import TextInput from "./components/TextInput";
 import Button from "./components/Button";
@@ -11,21 +10,21 @@ import CriteriaContainer from "./components/CrtieriaContainer";
 import Banner from "./components/Banner";
 import {debounce} from "lodash";
 
+const Container = styled.div({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  display: "grid",
+  placeContent: "center",
+});
 
 const LoginPage = (): JSX.Element => {
-  const Container = styled.div({
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    display: "grid",
-    placeContent: "center",
-  });
+  
   const ThreePixelVerticalSpacer = styled("div")({
     height: "3px",
   });
-  const isMobilePhone = isMobile && !isTablet;
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -35,7 +34,8 @@ const LoginPage = (): JSX.Element => {
   }
 
   const handleOnChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    debounce(() => setPassword(event.target.value), 10)();
+    const debouncedPasswordUpdate = debounce((password: string) => setPassword(password), 10)
+    debouncedPasswordUpdate(event.target.value);
 }
 
   return (
@@ -43,13 +43,13 @@ const LoginPage = (): JSX.Element => {
       <Banner>
         <Typography text={"Registration"} color={"white"} />
       </Banner>
-      <LoginCard isMobile={isMobilePhone}>
-        <InputColumn isMobile={isMobilePhone}>
+      <LoginCard>
+        <InputColumn>
           <Typography text={"Email"} />
           <TextInput type={'email'} onChange={handleOnChangeEmail} value={email}/>
         </InputColumn>
         <ThreePixelVerticalSpacer />
-        <InputColumn isMobile={isMobilePhone}>
+        <InputColumn>
           <Typography text={"Password"} />
           <TextInput type={'password'} onChange={handleOnChangePassword} value={password}/>
           <CriteriaContainer>
@@ -75,7 +75,6 @@ const LoginPage = (): JSX.Element => {
             />
           </CriteriaContainer>
           <Button
-            isMobile={isMobilePhone}
             onClick={() => {
               console.log(`email: ${email} password: ${password} 🎉🎉🎉🎉🎉`);
             }}
